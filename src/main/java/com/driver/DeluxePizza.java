28cc3f7d979f65815bce0adde3de9a7368b11111
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-01-17 19:08:36 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,18 @@
 package com.driver;
 
+// public class DeluxePizza extends Pizza {
+
+//     public DeluxePizza(Boolean isVeg) {
+//         super(isVeg);
+//         // your code goes here
+//     }
+// }
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
+    public DeluxePizza(int basePrice, boolean isVeg) {
+        super(basePrice, isVeg);
+        addExtraCheese();
+        addExtraToppings();
     }
 }
+
